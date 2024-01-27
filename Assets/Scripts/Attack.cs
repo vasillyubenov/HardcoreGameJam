e4dd15ceff25e66af21c829386815c68f4c5fbe7
borisI <ignboris2020@gmail.com>
2024-01-27 18:04:30 +0200

    Test Scene Setup

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -49,25 +49,3 @@ public class Attack : MonoBehaviour
         canAct = true;
     }
 }
-
-[CreateAssetMenu(fileName = "New Hit", menuName = "Create New Hit Properties")]
-public class HitData : ScriptableObject
-{
-    public Attack.AttackType attackType = Attack.AttackType.light;
-
-    [Header("Time Properties")]
-    public float windup = 0.1f;
-    public float duration = 0.1f;
-    public float recoil = 0.1f;
-
-    [Header("Impact Properties")]
-    public float range = 10f;
-    public float knockback = 10f;
-    public float damage = 10f;
-    public float stunTime = 0.5f;
-
-    public float GetAttackTime()
-    {
-        return windup + duration + recoil;
-    }
-}
--- Assets/Scripts/Prop.cs
+++ Assets/Scripts/Prop.cs
@@ -6,6 +6,6 @@ public class Prop : MonoBehaviour, IPunchable
 {
     public void GetHit(Attack.AttackType attack, float knockback, float damage, float stun)
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2 (knockback, Random.value * 0.5f);
+        GetComponent<Rigidbody2D>().velocity = new Vector2 (knockback, knockback);
     }
 }
