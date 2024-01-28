0b06b1792b3104be98c63db4a66b8d9f09a32c85
borisI <ignboris2020@gmail.com>
2024-01-28 11:43:08 +0200

    Props

--- Assets/Scripts/Prop.cs
+++ Assets/Scripts/Prop.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Prop : MonoBehaviour, IPunchable
 {
+    [SerializeField] private UnityEvent onHit;
     public void GetHit(Attack.AttackType attack, float knockback, float damage, float stun, int teamTag)
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2 (knockback, knockback);
+        GetComponent<Rigidbody2D>().AddForce(new Vector2 (knockback, knockback / 5f) * 30, ForceMode2D.Impulse);
+
+        onHit.Invoke();
     }
 }
